17bdfcbe6da7cbc7d5ca69e5a806b66427a1f650
Soumyajit Mandal <soumyajitm32@gmail.com>
2023-08-10 18:36:29 +0530

    User Profile Page With Address Editing-Added

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -8,54 +8,11 @@ import {
 import { Navigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import {
-  selectLoggedInUser,
   updateUserAsync,
 } from '../features/auth/authSlice';
 import { useState } from 'react';
 import { createOrderAsync, selectCurrentOrder } from '../features/order/orderSlice';
-
-const products = [
-    {
-      id: 1,
-      name: 'Throwback Hip Bag',
-      href: '#',
-      color: 'Salmon',
-      price: '$90.00',
-      quantity: 1,
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
-      imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
-    },
-    {
-      id: 2,
-      name: 'Medium Stuff Satchel',
-      href: '#',
-      color: 'Blue',
-      price: '$32.00',
-      quantity: 1,
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
-      imageAlt:
-        'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
-    },
-    // More products...
-  ]
-  const addresses = [
-    {
-    name: 'John wick',
-    street: '11th Main',
-    city: 'Delhi',
-    pinCode: 110001,
-    state: 'Delhi',
-    phone: 12312321331
-    },
-    {
-    name: 'John Doe',
-    street: '15th Main',
-    city: 'Bangalore',
-    pinCode: 560034,
-    state: 'Karnataka',
-    phone: 12312321339
-    },
-]
+import { selectUserInfo } from '../features/user/userSlice';
 
 const CheckoutPage = () => {
   const dispatch = useDispatch();
@@ -66,7 +23,7 @@ const CheckoutPage = () => {
     formState: { errors },
   } = useForm();
 
-  const user = useSelector(selectLoggedInUser);
+  const user = useSelector(selectUserInfo);
   const items = useSelector(selectItems);
   const currentOrder = useSelector(selectCurrentOrder);
 
